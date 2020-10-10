91e1006944998a7d7d9bb426613c0b6ed9d968cc
liplum <Li_plum@outlook.com>
2020-10-10 19:48:57 +0800

    Renamer

---
+++ SubWindows/RenamePhotosWindows/IRule.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PhotosCategorier.SubWindows.RenamePhotosWindows
+{
+    public interface IRule
+    {
+        string GenerateString();
+        static string RuleDisplayedName { get; }
+        Panel GetFuntionArea();
+
+    }
+}
